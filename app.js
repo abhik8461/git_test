90a14ec1744ab237d40ea3f78f6d4d877e8bfec4
abhik8461 <abhishek@mactosys.com>
2023-11-03 16:35:11 +0530

    new

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-import express from "express";
+// import express from "express";
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
-
+app.use(morgan("dev"));
 const port = 3900;
 
 let count = 0;
@@ -11,9 +13,21 @@ const countReq = (req, res, next) => {
   next();
 };
 
-app.get("/", countReq, (req, res) => {
+app.get("/count", countReq, (req, res) => {
   console.log(`Request Count:${count}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+  console.log("Hello World");
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/ejs", (req, res) => {
+  res.render("index");
+});
+
 app.listen(port, () => {
   console.log(`Server is Running at Port ${port}`);
 });
